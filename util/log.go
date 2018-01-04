2a728532a2c349ffa1b6abe111ba9d50d7934547
painterdrown <painterdrown@hotmail.com>
2018-01-04 20:23:05 +0800

    README

--- util/log.go
+++ util/log.go
@@ -10,7 +10,7 @@ var logger *log.Logger
 
 // InitLogger .
 func InitLogger(p, mode int) {
-	filename := "logs/" + strconv.Itoa(p) + "-" + strconv.Itoa(mode) + ".log"
+	filename := "./" + strconv.Itoa(p) + "-" + strconv.Itoa(mode) + ".log"
 	logFile, err := os.Create(filename)
 	CheckErr(err)
 	logger = log.New(logFile, "["+strconv.Itoa(p)+"]", log.LstdFlags)
